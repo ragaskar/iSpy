45392a0aee566444c5218531c69896fed6c777e0
ragaskar <ragaskar@gmail.com>
2009-06-17 13:16:22 -0700

    Added an untested matcher.

--- plugins/screw.iSpy.js
+++ plugins/screw.iSpy.js
@@ -2,13 +2,34 @@ Screw.Matchers.spyOn = function(obj, methodName) {
   return iSpy.spyOn(obj, methodName);
 };
 
+Screw.Matchers.be_called = {
+  match: function(expected, actual) {
+    console.debug(arguments);
+    return (this.actual && this.actual.isSpy && this.actual.wasCalled);
+  },
+
+  failure_message: function(expected, actual, not) {
+    if (!this.actual || !this.actual.isSpy) {
+      return 'Expected a spy, but got ' + actual + '.';
+    }
+    if (!actual.wasCalled) {
+      return 'Expected spy "' + this.actual.identity + '" to have been called, but it was not.';
+    }
+    return 'mystery fail!!!!';
+  }
+};
+
 (function($) {
-  $(Screw).bind("before", function(){
+  $(Screw).bind("before", function() {
     function removeAllSpies() {
       iSpy.removeAllSpies();
     }
 
-    $('.it').bind('passed', function(){ removeAllSpies(); });
-    $('.it').bind('failed', function(){ removeAllSpies(); });
+    $('.it').bind('passed', function() {
+      removeAllSpies();
+    });
+    $('.it').bind('failed', function() {
+      removeAllSpies();
+    });
   });
 })(jQuery);
